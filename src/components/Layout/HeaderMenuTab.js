ca2a235153ced43f369fab09af0c6c3abc91421d
guzdziac55 <guzdziac55@gmail.com>
2021-09-09 22:52:51 +0200

    aa

--- src/components/Layout/HeaderMenuTab.js
+++ src/components/Layout/HeaderMenuTab.js
@@ -2,14 +2,22 @@ import React from "react";
 import HeaderButtonMenu from "./HeaderButtonMenu";
 import classes from "./HeaderMenuTab.module.css";
 import IconLogin from "./IconLogin";
+import IconReport from "./IconReport";
+import IconSettings from "./IconSettings";
+
+const iconSrc = {
+  report: IconReport,
+  settings: IconSettings,
+  login: IconLogin,
+};
 
 const HeaderMenuTab = (props) => {
   return (
     <div className={classes["tab-menu"]}>
       {/* this is bad becouse we need to call  onClick on button */}
-      <HeaderButtonMenu icon={IconLogin} title={"Report"} />
-      <HeaderButtonMenu icon={IconLogin} title={"Settings"} />
-      <HeaderButtonMenu icon={IconLogin} title={"Login"} />
+      <HeaderButtonMenu icon={iconSrc.report} title={"Report"} />
+      <HeaderButtonMenu icon={iconSrc.settings} title={"Settings"} />
+      <HeaderButtonMenu icon={iconSrc.login} title={"Login"} />
     </div>
   );
 };
