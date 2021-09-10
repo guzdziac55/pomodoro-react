0ddb23285b7c39d1540ef5ff26c4f837c6e7de1d
guzdziac55 <guzdziac55@gmail.com>
2021-09-10 11:15:54 +0200

    add more features

--- src/components/Layout/HeaderButtonMenu.js
+++ src/components/Layout/HeaderButtonMenu.js
@@ -2,12 +2,7 @@ import React from "react";
 import classes from "./HeaderButtonMenu.module.css";
 
 const HeaderButtonMenu = (props) => {
-  return (
-    <button className={classes.button}>
-      <span>{props.icon}</span>
-      <span>{props.title}</span>
-    </button>
-  );
+  return <button className={classes.button}>{props.children}</button>;
 };
 
 export default HeaderButtonMenu;
--- src/components/Layout/HeaderMenuTab.js
+++ src/components/Layout/HeaderMenuTab.js
@@ -2,14 +2,22 @@ import React from "react";
 import HeaderButtonMenu from "./HeaderButtonMenu";
 import classes from "./HeaderMenuTab.module.css";
 import IconLogin from "./IconLogin";
+import classes from "./HeaderButtonMenu.module.css";
 
 const HeaderMenuTab = (props) => {
   return (
     <div className={classes["tab-menu"]}>
       {/* this is bad becouse we need to call  onClick on button */}
-      <HeaderButtonMenu icon={IconLogin} title={"Report"} />
-      <HeaderButtonMenu icon={IconLogin} title={"Settings"} />
-      <HeaderButtonMenu icon={IconLogin} title={"Login"} />
+      {/*  we can pass prop function into it  */}
+      <HeaderButtonMenu>
+        <span className={classes.icon}>
+          <IconLogin></IconLogin>
+        </span>
+        <span>asdasdsad</span>
+      </HeaderButtonMenu>
+      {/* <HeaderButtonMenu icon={<IconLogin />} title={"Report"} /> */}
+      <HeaderButtonMenu icon={<IconLogin />} title={"Settings"} />
+      <HeaderButtonMenu icon={<IconLogin />} title={"Login"} />
     </div>
   );
 };
