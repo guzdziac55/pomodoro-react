9f2a7a348639d9c081b76aea312c2a0ed8315d4c
guzdziac55 <guzdziac55@gmail.com>
2021-12-02 22:40:09 +0100

    profile update for avatar, username

--- src/store/profile-slice.js
+++ src/store/profile-slice.js
@@ -1,29 +1,46 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { createSelector } from "reselect";
+
+export const defaultProfileState = {
+  profileChanged: false,
+  userName: "defaultUserName",
+  avatarUrl: "default",
+};
 
 const profileSlice = createSlice({
   name: "profile",
   initialState: {
     profileChanged: false,
-    avatarUrl: "",
+    userName: "dawid",
+    avatarUrl: "dawid",
   },
   reducers: {
+    setProfile: (state, action) => {
+      return action.payload; //Zamiast tego, aby zastąpić istniejący stan, należy bezpośrednio zwrócić nową wartość:
+    },
+
+    setUserName(state, action) {
+      state.userName = action.payload;
+      state.profileChanged = true;
+    },
+
     setAvatarUrl(state, action) {
       state.avatarUrl = action.payload;
       state.profileChanged = true;
     },
+
     setProfileChanged(state) {
-      state.profileChanged = true;
+      // state.profileChanged = true;
     },
   },
 });
 
 // actions
-export const { setAvatarUrl } = profileSlice.actions;
+export const { setAvatarUrl, setUserName, setProfile } = profileSlice.actions;
 
 //selectors
-export const selectUserAvatar = (state) => state.profile.avatarUrl;
 export const selectUserProfile = (state) => state.profile;
+export const selectUserAvatar = (state) => state.profile.avatarUrl;
+export const selectUserName = (state) => state.profile.userName;
 export const selectProfieChanged = (state) => state.profile.profileChanged;
 
 export default profileSlice;
