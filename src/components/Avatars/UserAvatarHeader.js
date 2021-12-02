61001b53201f5909a0de6dd8160d1c422df38957
guzdziac55 <guzdziac55@gmail.com>
2021-12-02 23:03:27 +0100

    avatar header icon is now working with Our avatar API

--- src/components/Avatars/UserAvatarHeader.js
+++ src/components/Avatars/UserAvatarHeader.js
@@ -2,19 +2,16 @@ import React from "react";
 import classes from "./UserAvatarHeader.module.css";
 import { useSelector } from "react-redux";
 import { selectUserAvatar } from "../../store/profile-slice";
+import { generateAvatarURL } from "../../store/constants/app.constants";
 
 const UserAvatarHeader = ({ onClick }) => {
   const userAvatar = useSelector(selectUserAvatar);
 
   return (
     <div onClick={onClick} className={classes.avatarContainer}>
-      <img className={classes.avatar} src={userAvatar}></img>
+      <img className={classes.avatar} src={generateAvatarURL(userAvatar)}></img>
     </div>
   );
 };
 
 export default UserAvatarHeader;
-{
-  /* <MdQueryStats className={classes.icon} />
-        <span>Report</span> */
-}
