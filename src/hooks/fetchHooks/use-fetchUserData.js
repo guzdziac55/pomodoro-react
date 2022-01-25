0ac83005625c151d9b07b8b7f9a58f027f156be6
guzdziac55 <guzdziac55@gmail.com>
2022-01-25 22:13:41 +0100

    old setState WeekBoard before refactor

--- src/hooks/fetchHooks/use-fetchUserData.js
+++ src/hooks/fetchHooks/use-fetchUserData.js
@@ -7,9 +7,20 @@ const useFetchUserData = () => {
   const currentUser = useSelector(selectCurrentUser);
   const dispatch = useDispatch();
 
+  // calls after login
   useEffect(() => {
     if (currentUser) dispatch(fetchFirebaseUserData(currentUser.uid));
   }, [currentUser, dispatch]);
 };
 
 export default useFetchUserData;
+
+// clcik send
+// call dispatch send ?  or normall call sendFirebase async function
+// send object stateWeekplan to firebase
+
+// reaload
+// fetch data from weekplan
+// call useFetch userData in useEffect in weekPlan Page
+// put state down from props to useState initial ?
+// sprawdzićczy zadziała
--- src/store/thunks/taskList-actions.js
+++ src/store/thunks/taskList-actions.js
@@ -2,6 +2,8 @@ import { showNotification } from "../ui-slice";
 import { replaceTaskList, replaceTemplatesList } from "../taskList-slice";
 import { defaultConfigState } from "../config-slice";
 import { defaultProfileState } from "../profile-slice";
+import { defaultWeekPlan } from "../weekPlan-slice";
+import { setWeekPlan } from "../weekPlan-slice";
 import { setConfig } from "../config-slice";
 import { database } from "../../firebase";
 import { ref, update } from "firebase/database";
@@ -12,7 +14,68 @@ export {
   sendFirebaseTaskList,
   sendFirebaseSettings,
   sendFireBaseUserProfile,
+  // added Fetch
+  fetchFirebaseWeekPlan,
+  fetchFirebaseData,
   fetchFirebaseUserData,
+  //  new one
+  sendFirebaseWeekPlan,
+};
+
+// FIREBASE FETCH ASYNC WITHOUT DISPATCH
+
+const fetchFirebaseWeekPlan = async (uid) => {
+  const fetchRequest = async () => {
+    const dataRef = database.ref("users/" + uid + "/WeekPlan");
+    return dataRef;
+  };
+
+  try {
+    const dataRef = await fetchRequest();
+    const snapDataRef = await dataRef.once("value");
+    const data = snapDataRef.val();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const sendFirebaseWeekPlan = (weekPlan, uid) => {
+  return async (dispatch) => {
+    dispatch(
+      showNotification({
+        status: "pending",
+        title: "sending...",
+        message: "sending taskList Data",
+      })
+    );
+
+    const sendRequest = async () => {
+      update(ref(database, "users/" + uid), {
+        WeekPlan: weekPlan,
+      });
+    };
+
+    try {
+      console.log("TRY SEND TEMPLATES ! ");
+      await sendRequest();
+      dispatch(
+        showNotification({
+          status: "success",
+          title: "success...",
+          message: "send taskList data success",
+        })
+      );
+    } catch (err) {
+      dispatch(
+        showNotification({
+          status: "error",
+          title: "some error !",
+          message: "sending Tasklist data failed",
+        })
+      );
+    }
+  };
 };
 
 const sendFirebaseTemplates = (templates, uid) => {
@@ -167,6 +230,50 @@ const sendFireBaseUserProfile = (userProfile, uid) => {
   };
 };
 
+const fetchFirebaseData = (uid, destination) => {
+  return async (dispatch) => {
+    dispatch(
+      showNotification({
+        status: "loading",
+        title: "loading...",
+        message: "loading taskList Data",
+      })
+    );
+
+    const fetchRequest = async () => {
+      //  think about better functionality => without boilerPlate
+      const dataRef = database.ref("users/" + uid + `/${destination}`);
+      return [dataRef];
+    };
+
+    try {
+      const [dataRef] = await fetchRequest();
+      const snapshotData = await dataRef.once("value");
+      console.log(destination);
+      const dataValue = snapshotData.val();
+      console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+      console.log("data value wew");
+      console.log(dataValue);
+      return { dataValue };
+      dispatch(
+        showNotification({
+          status: "success",
+          title: "success...",
+          message: "Data loading success",
+        })
+      );
+    } catch (err) {
+      dispatch(
+        showNotification({
+          status: "error",
+          title: "some error !",
+          message: "loading Tasklist data failed",
+        })
+      );
+    }
+  };
+};
+
 const fetchFirebaseUserData = (uid) => {
   return async (dispatch) => {
     dispatch(
@@ -179,32 +286,39 @@ const fetchFirebaseUserData = (uid) => {
 
     const fetchRequest = async () => {
       //  think about better functionality => without boilerPlate
+      //  maybeFirebase HOOK for fetch ?
 
       const templatesRef = database.ref("users/" + uid + "/TasksTemplates");
       const tasksRef = database.ref("users/" + uid + "/TasksList");
       const settingsRef = database.ref("users/" + uid + "/Settings");
       const userProfileRef = database.ref("users/" + uid + "/UserProfile");
-      return [templatesRef, tasksRef, settingsRef, userProfileRef];
+      //  weekPlan
+      const weekPlanRef = database.ref("users/" + uid + "/WeekPlan");
+
+      return [templatesRef, tasksRef, settingsRef, userProfileRef, weekPlanRef];
     };
 
     try {
-      const [templatesRef, tasksRef, settingsRef, userProfileRef] =
+      const [templatesRef, tasksRef, settingsRef, userProfileRef, weekPlanRef] =
         await fetchRequest();
 
       const snapshotTemplates = await templatesRef.once("value");
       const snapshotTasks = await tasksRef.once("value");
       const snapshotSettings = await settingsRef.once("value");
       const snapshotUserProfile = await userProfileRef.once("value");
+      const snapshotWeekPlan = await weekPlanRef.once("value");
 
       const templatesListData = snapshotTemplates.val();
       const taskListData = snapshotTasks.val();
       const settingsData = snapshotSettings.val();
       const userProfileData = snapshotUserProfile.val();
-
+      const weekPlanData = snapshotWeekPlan.val();
+      // SET DATA TO REDUX STATES
       dispatch(replaceTemplatesList(templatesListData || []));
       dispatch(replaceTaskList(taskListData || []));
       dispatch(setConfig(settingsData || { ...defaultConfigState }));
       dispatch(setProfile(userProfileData || { ...defaultProfileState }));
+      dispatch(setWeekPlan(weekPlanData || { ...defaultWeekPlan }));
 
       dispatch(
         showNotification({
@@ -224,3 +338,42 @@ const fetchFirebaseUserData = (uid) => {
     }
   };
 };
+
+//  create Universall Creator for POST / SEND DATA
+// const universalSendAction = (dataToSend, uid, destination) => {
+//   return async (dispatch) => {
+//     dispatch(
+//       showNotification({
+//         status: "pending",
+//         title: "sending...",
+//         message: "sending taskList Data",
+//       })
+//     );
+
+//     const sendRequest = async () => {
+//       update(ref(database, "users/" + uid), {
+//         destination: dataToSend,
+//       });
+//     };
+
+//     try {
+//       console.log("TRY SEND TEMPLATES ! ");
+//       await sendRequest();
+//       dispatch(
+//         showNotification({
+//           status: "success",
+//           title: "success...",
+//           message: "send taskList data success",
+//         })
+//       );
+//     } catch (err) {
+//       dispatch(
+//         showNotification({
+//           status: "error",
+//           title: "some error !",
+//           message: "sending Tasklist data failed",
+//         })
+//       );
+//     }
+//   };
+// };
