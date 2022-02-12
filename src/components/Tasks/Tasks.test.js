dad04769b121503868d97c3f4aea1a62d8c80990
guzdziac55 <guzdziac55@gmail.com>
2022-02-12 16:47:18 +0100

    some clear comments

--- src/components/Tasks/Tasks.test.js
+++ src/components/Tasks/Tasks.test.js
@@ -2,12 +2,8 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import React from 'react'
 import { Provider } from 'react-redux'
-// import { Provider } from "react-redux";
 import store from '../../store/index'
 import Tasks from './Tasks'
-
-// put provider into external function
-// DRY
 
 describe('TASK component testing', () => {
     test('shows initial button to add Task / show form', () => {
@@ -43,19 +39,4 @@ describe('TASK component testing', () => {
 
         expect(buttonAdd).not.toBeInTheDocument()
     })
-
-    // v2
-
-    // test("hides button addTask after click on it ", () => {
-    //   render(
-    //     <Provider store={store}>
-    //       <Tasks />
-    //     </Provider>
-    //   );
-    //   const buttonAdd = screen.getByRole("button", { name: /add task/i });
-    //   expect(buttonAdd).toBeInTheDocument();
-    //   userEvent.click(buttonAdd);
-    //   const buttonAdd2 = screen.queryByRole("button", { name: /add task/i });
-    //   expect(buttonAdd2).toBeFalsy();
-    // });
 })
